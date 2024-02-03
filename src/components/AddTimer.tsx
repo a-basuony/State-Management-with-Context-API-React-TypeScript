078ab43ff11d2c6be3fcd6ff20b6ef2747a314af
a-basuony <A.Basuony.social@gmail.com>
2024-02-03 19:38:32 +0200

    using useTimerContext in Timers Components

--- src/components/AddTimer.tsx
+++ src/components/AddTimer.tsx
@@ -1,15 +1,22 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
-import Button from './UI/Button.tsx';
-import Form, { FormHandle } from './UI/Form.tsx';
-import Input from './UI/Input.tsx';
+import Button from "./UI/Button.tsx";
+import Form, { FormHandle } from "./UI/Form.tsx";
+import Input from "./UI/Input.tsx";
+import { useTimersContext } from "../store/timer-context.tsx";
 
 export default function AddTimer() {
   const form = useRef<FormHandle>(null);
-
+  const { addTimer } = useTimersContext();
   function handleSaveTimer(data: unknown) {
     const extractedData = data as { name: string; duration: string };
-    console.log(extractedData);
+    // const extractedData: {name: string; duration: string;} | +extractedDAta.duration
+    // duration as string here and it should be number So =>  parseInt(extractedData.duration, 10) or
+    addTimer({
+      name: extractedData.name,
+      duration: parseInt(extractedData.duration, 10),
+    });
+    // console.log(extractedData);
     form.current?.clear();
   }
 
--- src/components/Timers.tsx
+++ src/components/Timers.tsx
@@ -1,3 +1,17 @@
+import { useTimersContext } from "../store/timer-context";
+import Timer from "./Timer";
+
 export default function Timers() {
-  return <ul>{/* TODO ... */}</ul>;
+  const { timers } = useTimersContext();
+  return (
+    <ul>
+      {timers.map((timer) => (
+        <li key={timer.name}>
+          {/* <Timer name={timer.name} duration={timer.duration} /> */}
+          {/* spread timer data {...timer} */}
+          <Timer {...timer} />
+        </li>
+      ))}
+    </ul>
+  );
 }
